a6f0e09f7169d2b0e37c3c6f4d9b65167cab6722
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-08-10 11:43:21 +0530

    new

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,4 @@
-// import axios from "axios";
-// const URL =
-//   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-// export const getPlacesData = async (sw, ne) => {
-//   try {
-//     const {
-//       data: { data },
-//     } = await axios.get(URL, {
-//       params: {
-//         bl_latitude: sw.lat,
-//         tr_latitude: ne.lat,
-//         bl_longitude: sw.lng,
-//         tr_longitude: ne.lng,
-//       },
-//       headers: {
-//         "x-rapidapi-key": "a8497c2842msh1eba1e53ea39134p17a7e7jsnd41abbc0229f",
-//         "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-//       },
-//     });
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 
 import axios from "axios";
 
